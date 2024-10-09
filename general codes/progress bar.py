b90226bae85f919eda91e8173c9d0f1c59f847be
Sagar Dam <mr.sagardam@gmail.com>
2024-10-09 15:43:56 +0530

    CNN

--- general codes/progress bar.py
+++ general codes/progress bar.py
@@ -19,7 +19,7 @@ with tqdm(total=total_iterations,
           ncols=100, 
           bar_format=f"{{l_bar}}{GREEN}{{bar}}{RESET}|{{n_fmt}}/{{total_fmt}} [{GREEN}{{percentage:.0f}}%{RESET}]{{postfix}}") as pbar:  # Include postfix
     for i in range(total_iterations):
-        time.sleep(0.05)
+        time.sleep(0.01)
         # Update progress bar and set postfix to show additional info
         pbar.set_postfix({"Loss": f"{(100 - i) / 100:.4f}"})  # Example for loss
         pbar.update(1)
